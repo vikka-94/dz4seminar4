f8b2289126a07fd1f2137c5dc16e3b702733f19e
Vika <vikka-94@mail.ru>
2022-12-22 21:32:01 +0300

    dz4

---
+++ 29/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+//6, 1, 33 -> [6, 1, 33]
+
+
+Console.Write("Задайте элемент массива: ");
+int enterNumber = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[enterNumber];
+Console.Write("[");
+for (int i = 0; i < enterNumber; i++)
+{
+  array[i] = new Random().Next(enterNumber+1);
+  Console.Write(array[i] + ",");
+}
+Console.Write("]");
+Console.WriteLine();
+
